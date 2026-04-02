8b7e5335067959937e24ecd1bb23a6bb6cb627ce
Constraint-coder <mikelalas17@gmail.com>
2026-04-01 19:03:45 -0600

    plantilla

---
+++ app/Http/Controllers/CodigoBarraController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CodigoBarra;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CodigoBarraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CodigoBarra $codigoBarra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CodigoBarra $codigoBarra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CodigoBarra $codigoBarra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CodigoBarra $codigoBarra)
+    {
+        //
+    }
+}
