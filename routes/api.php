4bcf5f842a5e51b1b4337ff9eb0208d696f41b48
Constraint-coder <mikelalas17@gmail.com>
2026-04-11 16:02:31 -0600

    cambios en rutas api2

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RolController;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\VentaController;
+use App\Http\Controllers\DetalleVentaController;
+
+Route::apiResource('users', UserController::class);
+Route::apiResource('rols', RolController::class);
+Route::post('login', [AuthController::class, 'login']);
+Route::apiResource('productos', ProductoController::class);
+Route::apiResource('codigos-barra', CodigoBarraController::class);
